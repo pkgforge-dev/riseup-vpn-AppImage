88b873753c1b4dddcaeada1c00bd35892f8e1d17
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-10 15:32:39 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,27 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q riseup-vpn | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/128x128/apps/riseup-vpn.svg
+export DESKTOP=/usr/share/applications/riseup-vpn.desktop
+export DEPLOY_SYS_PYTHON=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/riseup-vpn \
+	/usr/bin/bitmask-root
 
-# Additional changes can be done in between here
+dst=./AppDir/share/polkit-1/actions
+mkdir -p "$dst"
+cp -v /usr/share/polkit-1/actions/se.leap.bitmask.policy "$dst"
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
 # Test the app for 12 seconds, if the app normally quits before that time
 # then skip this or check if some flag can be passed that makes it stay open
-quick-sharun --test ./dist/*.AppImage
+#quick-sharun --test ./dist/*.AppImage
