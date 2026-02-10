79a12a883e9907f76b065eb7fba7004264c2350c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-10 15:40:29 -0400

    fix

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/128x128/apps/riseup-vpn.svg
+export ICON=/usr/share/icons/hicolor/128x128/apps/riseup-vpn.png
 export DESKTOP=/usr/share/applications/riseup-vpn.desktop
 export DEPLOY_SYS_PYTHON=1
 
